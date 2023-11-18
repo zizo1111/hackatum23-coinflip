9f7ec5e4f7efaf8e58e5d7ca698231a78c34401f
= Enea_Gore <ge72git@mytum.de>
2023-11-19 00:21:49 +0100

    Update the Home page to initialize Qdrant database

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -1,46 +1,21 @@
 import streamlit as st
 from PIL import Image
+import os
+import time
+
+st.write("# Welcome to the CoinFlip Log Analyser")
+
+### XXX  Load the vetor database Qdrant in memory
+
+with st.spinner("Initilaizing in-memory Qdrant vector database for the session"):
+    from sentence_transformers import SentenceTransformer
+    from qdrant_client import QdrantClient, models
+    from qdrant_client.models import PointStruct
+    client  = QdrantClient(":memory:")
+    client.create_collection("my_collection", vectors_config=models.VectorParams(size=384, distance=models.Distance.COSINE) )
+    st.session_state["client"] = client
+    st.session_state["model"] = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
 
-st.set_page_config(page_title="Streamlit Code Snippets",
-                   page_icon="👋",
-                   layout='wide')
-
-st.write("# Welcome to the Streamlit Code Snippet 👋")
-st.write("We start the app by runnning the home page with:")
-st.code("streamlit run Home.py")
-
-st.write(
-    "The app project structure contains the main page py file, and a folder called pages, with other subpages py files."
-)
-
-image = Image.open('snippet_tree.png')
-
-st.image(image)
-
-st.write(
-    "They all are rendered in the navigation panel on top of the sidebar.")
-st.write(
-    "Make sure to name the subpages correctly by indicating the order on the beginning of the name."
-)
-
-st.write(
-    "All pages can set their title, icon and default layout format (wide or centered) plus other default behaviour (initial sidebar state, menu items...)"
-)
-st.code(""" 
-import streamlit as st
-
-# As first st command in the page:
-st.set_page_config(page_title="Streamlit Code Snippets",
-                       page_icon="👋",
-                       layout='wide')
-
-st.write(......)
-
-""")
-
-st.sidebar.success("Select a page above.")
-
-st.markdown("""
-    Navigate throught the different pages on the navigation panel on the left
-""")
+st.sidebar.success("Database and model loaded in memory")
+st.write("Qdrant Database initiliaized in memory")
