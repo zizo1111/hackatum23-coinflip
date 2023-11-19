9774b382557aaaf2d1bb52edde20ab2c360dd769
= Enea_Gore <ge72git@mytum.de>
2023-11-19 03:44:32 +0100

    Update log analysis and home

--- streamlit/Home.py
+++ streamlit/Home.py
@@ -3,7 +3,7 @@ from PIL import Image
 import os
 import time
 
-st.write("# Welcome to the CoinFlip Log Analyser")
+st.title("CoinFlip Log Analyser")
 
 ### XXX  Load the vetor database Qdrant in memory
 
@@ -19,3 +19,73 @@ with st.spinner("Initilaizing in-memory Qdrant vector database for the session")
 
 st.sidebar.success("Database and model loaded in memory")
 st.write("Qdrant Database initiliaized in memory")
+
+uploaded_file = st.file_uploader("Upload your Log file",type=['.txt',".out"])
+
+if uploaded_file is not None and "file" not in st.session_state:
+    st.session_state["file"] = uploaded_file
+    st.success("File uploaded successfully")
+
+# uploaded_file = st.file_uploader("Upload your Log file",type=['.txt',".out"])
+# lines = None
+# clean_lines = []
+# st.header('Log Analyser')
+
+# tab1, tab2, tab3, tab4 = st.tabs(['File Information', 'Questions and Answers', 'Pre-processing', "Empty containers"])
+
+# tab1.subheader('File Information')
+
+
+
+# with tab1:
+#     sum_chars = 0
+#     clean_chars = 0 
+#     if uploaded_file is not None:  
+#             st.sidebar.success("File uploaded successfully")
+#             name = uploaded_file.name
+#             lines = uploaded_file.readlines()
+#             size = uploaded_file.size
+#             st.write("Filename: ", name)
+#             st.write("File size: ", round(size/ (1024 * 1024),3) ,"MB")
+#             st.write("Number of log entries: " , len(lines))
+#             st.session_state.read_file = True
+#             with st.spinner("Pre-processing file"):
+#                 with st.spinner("Processing file "):
+#                     for line in lines:
+#                         sum_chars += len(line)
+#                         line = line.replace(line.split(b': ',2)[0],b'')
+#                         #print(line)
+#                         clean_chars += len(line)
+#                         clean_lines.append(str(line))
+#                 st.write("Total character sum before processing: ",sum_chars)
+#                 st.write("Total character sum after processing : ",clean_chars)
+#             with st.spinner("Caluclating embedings"):
+#                 embeddings = model.encode(clean_lines)
+
+
+
+# with tab2:
+#     res = st.text_area("Ask your questions here")
+#     if res is not None :
+#         st.write(res)
+
+# tab3.subheader('Pre processing')
+
+# tab4.subheader('Empty containers')
+
+# with tab4:
+#     with st.echo():
+#         st.write("You can create placeholders and empty them when desired")
+#         placeholder = st.empty()
+#         fill_button = st.button("Fill the empty container")
+#         clean_button = st.button("Empty the container")
+#         if fill_button:
+#             placeholder.subheader("We are filling something here")
+#         if clean_button:
+#             placeholder.empty()
+
+#         st.info("This must appear below the container")
+
+
+
+# st.sidebar.success("Select a page above.")
--- streamlit/pages/01_Log_Analytics.py
+++ streamlit/pages/01_Log_Analytics.py
@@ -2,15 +2,18 @@ import streamlit as st
 from qdrant_client.models import PointStruct
 import time
 st.set_page_config(page_title="Log Analyser", page_icon="👋", layout='wide')
+from difflib import SequenceMatcher
+import pandas as pd
 
+if "file" not in st.session_state:
+    st.error("Upload a file before proceeding")
+#uploaded_file = st.file_uploader("Upload your Log file",type=['.txt',".out"])
 
-uploaded_file = st.file_uploader("Upload your Log file",type=['.txt',".out"])
 num_of_lines_to_embedd = st.slider("Lines to Embedd", min_value=500, max_value=70000, value=1000, step=100)
 num_of_chunks = st.slider("Lines to Embedd", min_value=10, max_value=1000, value=20, step=10)
 
-lines = None
+lines = []
 clean_lines = []
-to_encode = []
 st.header('Log Analyser')
 more_embed = []
 
@@ -25,19 +28,20 @@ if "model" not in st.session_state or "client not in st.session_state":
         st.session_state["client"] = client
         st.session_state["model"] = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 
-tab1, tab2, tab3, tab4 = st.tabs(['File Information', 'Questions and Answers', 'Pre-processing', "Empty containers"])
+tab1, tab2= st.tabs(['File Information', 'Log Explorer'])
 
 tab1.subheader('File Information')
 
-def calc_embed(hits):
-        care = hits[0].payload["Inhalt"].split(";;;")
+# Calculates embeddings for less
+def calc_embed(inhalt):
         start = time.time()
-        more_embed = st.session_state.model.encode(care)
+        more_embed = st.session_state.model.encode(inhalt)
         end = time.time()
         return more_embed
         #st.write("Finished embeddings in " ,end-start)
 
-def calculate_embeddings():
+#Calculates Embeddings for all
+def calculate_embeddings(to_encode):
     with st.spinner("Calculating embeddings, please wait before proceeding"):
         start = time.time()
         st.session_state["embeddings"] = st.session_state.model.encode(to_encode)
@@ -48,10 +52,12 @@ def calculate_embeddings():
 with tab1:
     sum_chars = 0
     clean_chars = 0 
-    if uploaded_file is not None:  
-            st.sidebar.success("File uploaded successfully")
+    to_encode = []
+    if "file" in st.session_state:
+            uploaded_file = st.session_state.file 
             name = uploaded_file.name
             lines = uploaded_file.readlines()
+            st.session_state["lines"] = lines
             size = uploaded_file.size
             st.write("Filename: ", name)
             st.write("File size: ", round(size/ (1024 * 1024),3) ,"MB")
@@ -59,36 +65,36 @@ with tab1:
             st.session_state.read_file = True
             with st.spinner("Pre-processing file"):
                 with st.spinner("Removing unnecesary characters"):
-                    for line in lines:
+                    for line in st.session_state.lines:
                         sum_chars += len(line)
-                        if "Got Data" not in str(line):
+                        #if "Got Data" not in str(line):
                             
-                            line = line.replace(line.split(b': ',2)[0],b'')
-                            #print(line)
-                            clean_chars += len(line)
-                            end_line = str(line).replace("b'", "").replace("\\r","").replace("\\n","").replace(": ","")
-                            clean_lines.append(str(end_line))
+                        line = line.replace(line.split(b': ',2)[0],b'')
+                        #print(line)
+                        clean_chars += len(line)
+                        end_line = str(line).replace("b'", "").replace("\\r","").replace("\\n","").replace(": ","")
+                        clean_lines.append(str(end_line))
+
                 st.write("Total character sum before processing: ",sum_chars)
                 st.write("Total character sum after processing : ",clean_chars)
                 st.write("Total line sum before processing: ",len(lines))
                 st.write("Total line sum after processing : ",len(clean_lines))
 
-
+                
                 for i in range(0,num_of_lines_to_embedd, num_of_chunks):
                     to_encode.append(" ;;; ".join(clean_lines[i:i+num_of_chunks]))
             if "calculated" not in st.session_state:
-                calculate_embeddings()
+                calculate_embeddings(to_encode)
             elif(st.session_state.calculated == False):
-                calculate_embedings()
+                calculate_embedings(to_encode)
             
                 #st.write(res)
-tab2.subheader('Controls in the Sidebar, effects in the columns')
 
 
-
-def search_vector(query_vector,hits):
-        inhalt = hits[0].payload["Inhalt"].split(";;;")
-        embed = calc_embed(hits)
+def search_vector(query_vector,hit):
+        big_id = hit.id
+        inhalt = hit.payload["Inhalt"].split(";;;")
+        embed = calc_embed(inhalt)
 
         st.session_state.client.upsert(
                     collection_name="second_coll",
@@ -96,7 +102,7 @@ def search_vector(query_vector,hits):
                 PointStruct(
                         id=idx,
                         vector=vector.tolist(),
-                        payload={"Inhalt": inhalt[idx]}
+                        payload={"Inhalt": inhalt[idx],"Line_Count": idx + big_id*num_of_chunks+1}
                 )
             for idx, vector in enumerate(embed)
         ]
@@ -104,13 +110,11 @@ def search_vector(query_vector,hits):
         hits = st.session_state.client.search(
         collection_name="second_coll",
         query_vector=query_vector,
-        limit=10  # Return 10 closest points
+        limit=20  # Return 10 closest points
         )
-        print("HITS")
-        print(hits)
+        return hits
 
-
-def search_hits(query_vector):
+def search_hits(query_vector,to_encode):
         st.session_state.client.upsert(
                     collection_name="my_collection",
             points=[
@@ -130,17 +134,22 @@ def search_hits(query_vector):
         print(hits[0].id)
 
         for hit in hits:
-            st.header("Hit number: " + str(hit.id) + " with score " + str(hit.score))
-            st.write(hit.payload["Inhalt"])
-        
+            st.header("Highest vector similiarity in lines: " + str(hit.id * num_of_chunks)+"-"+str(hit.id * num_of_chunks +num_of_chunks) + " with score " + str(hit.score))
+            st.subheader("Top Results of this chunk")
+            hits_0 = search_vector(query_vector,hit)
+            for hit in hits_0:
+                idx = hit.payload["Line_Count"]
+                if hit.score < 0.5:
+                    st.write(":red[Line:] " +str(idx) + " :blue[Message:] " +str(lines[idx]).replace("b'", "").replace("\\r","").replace("\\n",""))
+                else:
+                    st.warning(":red[Line:] " +str(idx) + " :blue[Message:] " +str(lines[idx]).replace("b'", "").replace("\\r","").replace("\\n",""), icon="⚠️")
 
-        search_vector(query_vector,hits)
-        
-        #st.write(hits)
-
+# Find if there is a similar output
+def find_similar_out(arr, element):
+    SequenceMatcher(None, a, b).ratio()
 
 with tab2:
-    res = st.text_area(label = "Ask your questions here", value = "Which logs have errors or connection problems")
+    res = st.text_area(label = "Enter keywords youre interested in", value = "SSH connection error CMX")
     query_vector = st.session_state.model.encode(res.replace("log",""))
     if "embeddings" in st.session_state:
-        search_hits(query_vector)
+        search_hits(query_vector,to_encode)
